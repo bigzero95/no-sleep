2604edf841ac65111d5c085a5d9441395407f5f0
pdy <bigzero95@naver.com>
2020-11-22 23:37:56 +0900

    3주차...

--- main.py
+++ main.py
@@ -8,20 +8,37 @@
     #    if time.time() > max_time_end:
      #       break
              
+from playsound import playsound
 
 import random
 import time
 
-x = int(input('time(second) : '))
+playsound("방사능 위험 알람소리.mp3" ) 
 
-my_num = 3
+x = int(input("도착 소요 시간 : "))
+i = int(input("졸림 정도 : " ))  
 counter = x
+print ("졸음 방지 프로그램을 실행합니다.")
 
 start_time = time.time()
-while time.time() - start_time < counter:
-     if round(time.process_time()) == my_num:
-          print(random.randint(0,100))
-          time.sleep(3)
+while time.time() - start_time < counter:     
+     if round(time.process_time()) == i:
+          print(random.randint(0,100))  #랜덤 알람 대체
+          time.sleep(1)
+
+     #elif 
+      #    n = input('졸림정도 : ')
+       ## break
+
+
+
+     
+    
+
+
+
+        
+
 
 #import random
 #import time
