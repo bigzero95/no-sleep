abc10eaa0d5610a29f7d4b41cd68f48806e21085
pdy <bigzero95@naver.com>
2020-11-29 23:17:30 +0900

    4주차 졸음운전방지

--- main.py
+++ main.py
@@ -8,25 +8,37 @@
     #    if time.time() > max_time_end:
      #       break
              
-from playsound import playsound
+from pygame import mixer
 
 import random
 import time
+import pygame
 
-playsound("방사능 위험 알람소리.mp3" ) 
 
 x = int(input("도착 소요 시간 : "))
 i = int(input("졸림 정도 : " ))  
-counter = x
+
 print ("졸음 방지 프로그램을 실행합니다.")
 
 start_time = time.time()
-while time.time() - start_time < counter:     
+while time.time() - start_time < x:      
      if round(time.process_time()) == i:
-          print(random.randint(0,100))  #랜덤 알람 대체
-          time.sleep(1)
+          print ("버튼을 누르시오")
+          pygame.mixer.init()
+          pygame.mixer.music.load("C:\\Users\\AimToG\\Documents\\AlarmClockBell_S08HS.9.wav")
+          pygame.mixer.music.play(-1)
+          i = int(i)+int(input("졸림 정도 : " ))
+          continue
 
-     #elif 
+#if i <= 3     
+        
+
+     #y = input('입력 : ' )
+
+    # if y == ' ':
+          #pygame.mixer.music.pause()
+     #pygame.mixer.music.unpause()
+
       #    n = input('졸림정도 : ')
        ## break
 
