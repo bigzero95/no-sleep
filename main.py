7e88f74fe0867d618a5f2f2e6c82fd8cdadfdb8b
pdy <bigzero95@naver.com>
2020-12-03 00:30:36 +0900

    수요일 중간

--- main.py
+++ main.py
@@ -9,26 +9,52 @@
      #       break
              
 from pygame import mixer
-
+import os
 import random
 import time
 import pygame
 
+path = "C:\\Users\\AimToG\\Downloads\\졸음운전방지\\random_sound"
 
-x = int(input("도착 소요 시간 : "))
-i = int(input("졸림 정도 : " ))  
+x = int(input("도착 소요 시간(00분) : "))
+x_2 = x*60
 
-print ("졸음 방지 프로그램을 실행합니다.")
+i = int(input("졸림 정도(1~3단계) : " ))  
+i_2 = i*6
+
+#while i <= 1111 :
+ #    if (i>=1 and i<=3) : 
+  #        print ("졸음 방지 프로그램을 실행합니다.")
+   #       break
+    # else :
+     #     print ("잘못된 입력입니다.")
+      #    continue
+
+while i>=1 and i<=3 :
+     print ("졸음 방지 프로그램을 실행합니다.")
+     break
+     if i<=0 and i>=4 :
+          print ("잘못된 입력입니다.")
+          continue
 
 start_time = time.time()
-while time.time() - start_time < x:      
-     if round(time.process_time()) == i:
-          print ("버튼을 누르시오")
+while time.time() - start_time < x_2:      
+     if round(time.process_time()) == i_2:
+          print ("졸음 방지 알람입니다. 종료하시려면 버튼을 누르세요")
+          soundtrack = os.path.join(path, random.choice(os.listdir(path)))
           pygame.mixer.init()
-          pygame.mixer.music.load("C:\\Users\\AimToG\\Documents\\AlarmClockBell_S08HS.9.wav")
+          pygame.mixer.music.load(soundtrack)
           pygame.mixer.music.play(-1)
-          i = int(i)+int(input("졸림 정도 : " ))
-          continue
+          i = int(i)+int(input("졸림 정도(1~3단계) : " ))
+          i_2 = i*6
+          pygame.mixer.music.stop()
+          continue  
+
+
+
+
+
+
 
 #if i <= 3     
         
