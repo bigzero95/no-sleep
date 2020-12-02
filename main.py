d315a4009b9512f2c1c004b5fd84d812dbd3fd99
pdy <bigzero95@naver.com>
2020-12-03 01:31:59 +0900

    gui도입

--- main.py
+++ main.py
@@ -18,25 +18,18 @@ path = "C:\\Users\\AimToG\\Downloads\\졸음운전방지\\random_sound"
 
 x = int(input("도착 소요 시간(00분) : "))
 x_2 = x*60
+h = 5
 
-i = int(input("졸림 정도(1~3단계) : " ))  
-i_2 = i*6
-
-#while i <= 1111 :
- #    if (i>=1 and i<=3) : 
-  #        print ("졸음 방지 프로그램을 실행합니다.")
-   #       break
-    # else :
-     #     print ("잘못된 입력입니다.")
-      #    continue
-
-while i>=1 and i<=3 :
-     print ("졸음 방지 프로그램을 실행합니다.")
-     break
-     if i<=0 and i>=4 :
+while h <= 1111 :
+     i = int(input("졸림 정도(1~3단계) : " ))  
+     if (i>=1 and i<=3) : 
+          print ("졸음 방지 프로그램을 실행합니다.")
+          break
+     else :
           print ("잘못된 입력입니다.")
           continue
 
+i_2 = i*6
 start_time = time.time()
 while time.time() - start_time < x_2:      
      if round(time.process_time()) == i_2:
